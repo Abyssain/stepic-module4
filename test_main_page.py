3a676dddfbf29d6514dd46ef378dc80a305e29c5
Margarita <snetkova28@gmail.com>
2019-10-15 04:01:49 +0300

    Realised new method with pageswap

--- test_main_page.py
+++ test_main_page.py
@@ -4,9 +4,11 @@ from Pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page() 
 
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
